af67c0e0776b2f2d9da9ecf5ddf68b693085dcf7
Your Name <you@example.com>
2026-02-27 11:40:36 +0100

    node data struct

--- mesh_node/src/main.cpp
+++ mesh_node/src/main.cpp
@@ -1,11 +1,34 @@
 #include <Arduino.h>
 
+#define MAX_NEIGHBORS 5 
+#define MAX_APPROVED_EDGE 10
+#define MAX_PAYLOAD 8
+
+def struct{
+  uint8_t node_id;
+  uint8_t parent_id;
+  uint8_t node_depth; //ex. if not == 1, sent to parent (never same depth if parent not dead). if switch it updates depth
+  uint32_t last_parent_heartbeat;
+  
+  uint8_t approved_neighbors[MAX_NEIGHBORS]; // curent approved node devices
+  uint8_t approved_neighbors_count;
+
+  uint8_t neighbor_table[MAX_NEIGHBORS]; // secondary device routs
+  uint8_t neighbor_count;
+
+  uint8_t approved_list[MAX_APPROVED_EDGE]; // curent approved edge devices
+  uint8_t approved_count;
+  
+  uint8_t payload[MAX_PAYLOAD];
+  uint8_t payload_len;
+}node_data_t;
+
 // put function declarations here:
-int myFunction(int, int);
+//int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  //int result = myFunction(2, 3);
 }
 
 void loop() {
@@ -13,6 +36,8 @@ void loop() {
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+//int myFunction(int x, int y) {
+  //return x + y;
+//}
+
+
