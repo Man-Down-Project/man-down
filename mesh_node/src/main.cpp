e78d21e593b9dbfdd9103f01e367a5b77f649f39
Your Name <you@example.com>
2026-02-27 13:48:33 +0100

    test setup and loop

--- mesh_node/src/main.cpp
+++ mesh_node/src/main.cpp
@@ -1,10 +1,11 @@
 #include <Arduino.h>
+#include <ArduinoBLE.h>
 
 #define MAX_NEIGHBORS 5 
 #define MAX_APPROVED_EDGE 10
 #define MAX_PAYLOAD 8
 
-def struct{
+typedef struct{
   uint8_t node_id;
   uint8_t parent_id;
   uint8_t node_depth; //ex. if not == 1, sent to parent (never same depth if parent not dead). if switch it updates depth
@@ -23,32 +24,27 @@ def struct{
   uint8_t payload_len;
 }node_data_t;
 
-// put function declarations here:
-//int myFunction(int, int);
+node_data_t my_node;
 
 void setup() {
-  // put your setup code here, to run once:
-  //int result = myFunction(2, 3);
+
   Serial.begin(115200);
 
   my_node.node_id = 1;
   my_node.parent_id = 0;
   my_node.node_depth = 1;
-  my_node.last_parent_heartbeat_ms = millis();
-  my_node.node_id = 1;
-  my_node.parent_id = 0;
-  my_node.node_depth = 1;
-  my_node.last_parent_heartbeat_ms = millis();
+  my_node.last_parent_heartbeat = millis();
   my_node.payload[0] = 0xA5;
   my_node.payload_len = 1;
 
-
-  Serial.println("Node initialized!");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-    my_node.last_parent_heartbeat_ms = millis();
+   // my_node.last_parent_heartbeat = millis();
+    Serial.print("node_id: ");
+    Serial.println(my_node.node_id);
+    Serial.println("Looping...");
     delay(1000);
 }
 
