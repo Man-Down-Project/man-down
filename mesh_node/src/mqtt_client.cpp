a4efd4af8b0a9414731e55ede47d1ea03ed607c1
Your Name <you@example.com>
2026-03-02 16:25:22 +0100

    change sice of mqtt pack

--- mesh_node/src/mqtt_client.cpp
+++ mesh_node/src/mqtt_client.cpp
@@ -44,7 +44,7 @@ void mqtt_init() {
 bool mqtt_publisher_edge_event(edge_event_t* pkt) {
     return mqttClient.publish(
         topic,
-        (uint8_t*)pkt,
+        (uint16_t*)pkt,
         sizeof(edge_event_t)
     );
 }
