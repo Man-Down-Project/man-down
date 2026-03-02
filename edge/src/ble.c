f30d9ef57cd4fe5120045a218c61c4ca1cbb68ca
sacharias götesson <sachariasgj@gmail.com>
2026-03-02 11:32:45 +0100

    Kommenterade ut lite gammal kod som inte används

--- edge/src/ble.c
+++ edge/src/ble.c
@@ -212,9 +212,9 @@ void ble_init()
 // Enable bonding (store keys in NVS)
     ble_hs_cfg.sm_bonding = 1;
 
-// No MITM (not sure what this does yet. on = 1 off = 0)
+// No MITM (not sure what this does yet. guessing on = 1 off = 0)
 // defaults back to legacy if not supported by the device
-    ble_hs_cfg.sm_mitm = 1;
+    ble_hs_cfg.sm_mitm = 0;
 
 // No input/output capability
     ble_hs_cfg.sm_io_cap = BLE_HS_IO_NO_INPUT_OUTPUT;
--- edge/src/ble_gatt_client.c
+++ edge/src/ble_gatt_client.c
@@ -88,19 +88,19 @@ static int gatt_chr_cb(uint16_t conn_handle,
         ESP_LOGI(TAG, "Characteristic discovery complete");
 
         if (target_char_handle != 0) {
-            const char *msg = "HEARTBEAT"; // just here for testing
+            //const char *msg = "HEARTBEAT"; // just here for testing
             ble_gattc_disc_all_dscs(conn_handle,
-                                chr->val_handle,
+                                target_char_handle,
                                 service_end_handle,
                                 gatt_dsc_cb,
                                 NULL);
 
-            ble_gattc_write_flat(conn_handle,
-                                target_char_handle,
-                                msg,
-                                strlen(msg),
-                                gatt_write_cb,
-                                NULL);
+            // ble_gattc_write_flat(conn_handle,
+            //                     target_char_handle,
+            //                     msg,
+            //                     strlen(msg),
+            //                     gatt_write_cb,
+            //                     NULL);
         } else {
             ESP_LOGE(TAG, "Target characteristic not found!");
         }
