c93afe0b912d781e952fb8cb723197a50a2650d5
sacharias götesson <sachariasgj@gmail.com>
2026-03-04 15:16:33 +0100

    Think i managed to fix roaming for edge device (might still be a bug since it got stuck once after a disconnet, but could not reproduce bug)

--- mesh_node/src/config.hpp
+++ mesh_node/src/config.hpp
@@ -4,12 +4,12 @@
 #define NODE_ID 1
 
 //wifi config
-#define WIFI_SSID "wifi name"
-#define WIFI_PASS "wifi password"
+#define WIFI_SSID "Tele2_333f71_2.4Ghz"
+#define WIFI_PASS "qdzjtnwi"
 
 //MQTT config
-#define MQTT_BROKER "ip"
-#define MQTT_PORT 8883
+#define MQTT_BROKER "192.168.0.2"
+#define MQTT_PORT 1883
 
 //Topics
 #define MQTT_TOPIC_EVENT "mesh/edge_event"
