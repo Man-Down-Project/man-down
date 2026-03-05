14decdceb25191a3b20b60c6eb96140b36a43a32
Your Name <you@example.com>
2026-03-05 10:11:39 +0100

    personal setup and tempalet fix

---
+++ mesh_node/src/personal_setup_template.hpp
@@ -0,0 +1,12 @@
+///--------------------------------------------------------------------------
+///Uppdate ure local personal_setup.hpp file
+///--------------------------------------------------------------------------
+
+
+//wifi config
+#define WIFI_SSID "WiFi name"
+#define WIFI_PASS "WiFi password"
+
+//MQTT config
+#define MQTT_BROKER "192.168.0.2"
+#define MQTT_PORT 1883
