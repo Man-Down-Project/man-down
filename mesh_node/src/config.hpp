302c8fc20cc56f653ad65e61958fc066472eca19
Your Name <you@example.com>
2026-03-05 09:55:24 +0100

    test

--- mesh_node/src/config.hpp
+++ mesh_node/src/config.hpp
@@ -4,13 +4,7 @@
 //node config
 #define NODE_ID 1
 
-//wifi config
-#define WIFI_SSID "Tele2_333f71_2.4Ghz"
-#define WIFI_PASS "qdzjtnwi"
 
-//MQTT config
-#define MQTT_BROKER "192.168.0.2"
-#define MQTT_PORT 1883
 
 //Topics
 #define MQTT_TOPIC_EVENT "mesh/edge_event"
