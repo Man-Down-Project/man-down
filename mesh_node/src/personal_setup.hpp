6b45998605ff24bcf46f750d5411c8ab536e3c17
Your Name <you@example.com>
2026-03-05 09:56:55 +0100

    pesonal setup fix

--- mesh_node/src/personal_setup.hpp
+++ mesh_node/src/personal_setup.hpp
@@ -1,6 +1,6 @@
 //wifi config
-#define WIFI_SSID "COMFAST_9F9A_2G"
-#define WIFI_PASS "Kafademapa7078"
+#define WIFI_SSID "Wifi name"
+#define WIFI_PASS "pasword"
 
 //MQTT config
 #define MQTT_BROKER "192.168.0.2"
