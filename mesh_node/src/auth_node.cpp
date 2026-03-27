84bb06ac043f8358113a98d0497e2ff6085c1f9c
Your Name <you@example.com>
2026-03-27 12:17:48 +0100

    restore

--- mesh_node/src/auth_node.cpp
+++ mesh_node/src/auth_node.cpp
@@ -4,6 +4,7 @@
 #include <stddef.h>
 #include "auth_node.hpp"
 
+
 AuthNode authNode;
 
 AuthNode::AuthNode() {
@@ -17,45 +18,30 @@ AuthNode::AuthNode() {
 }
 
 
-//test start
-void AuthNode::begin(uint8_t node_id) { //fake for key test, shold be remove later
-
-    _node_id = node_id;
-
-    // Clear all authorized edges first
-    for (int i = 0; i < MAX_APPROVED_EDGE; i++) {
-        _authorized_edge[i].device_id = 0xFF;  // mark unused
-        _authorized_edge[i].last_seq = 0xFF;
-        _authorized_edge[i].key_timestamp = 1;
+bool AuthNode::isStorageEmpty(){
+    for(int i = 0; i < MAX_APPROVED_EDGE; i++){
+        if(_authorized_edge[i].device_id != 0xFF){
+            return false;
+        }
     }
-
-    // ----- FAKE FOG PROVISIONING -----
-
-    // We authorize ONE edge device (device_id = 1)
-    _authorized_edge[0].device_id = 1;
-
-    uint8_t fake_key[KEY_LEN] = {
-        0x9A, 0x4F, 0x21, 0xC7,
-        0x55, 0x13, 0xE8, 0x02,
-        0x6D, 0xB9, 0x33, 0xA1,
-        0x7C, 0x4D, 0x90, 0xEE
-    };
-
-    memcpy(_authorized_edge[0].shared_key, fake_key, KEY_LEN);
-
-    _authorized_edge[0].last_seq = 0xFF;
-
-    Serial.println("Edge provisioned (device_id = 1)");
+    return true;
 }
-//test end
 
 
-/*
 void AuthNode::begin(uint8_t node_id) {  //This is the live code
     _node_id = node_id;
+
     loadAuthorizedEdges();
+
+    if(isStorageEmpty()){
+        Serial.println("Storage is empty.. Attempting provisioning...");
+        //maby force a frist provisioning from mqtt topic here?
+       
+    }else{
+        Serial.println("Storage already initialized");
+    }
 }
-*/
+
 
 
 void AuthNode::loadAuthorizedEdges() { //live code
@@ -88,16 +74,9 @@ bool AuthNode::validateEdge(edge_event_t* pkt) {
                 return false; // not provisioned
             }
             
-            if (_authorized_edge[i].last_seq == seq) // just for demo, remove later //kolla att köra på timestapm ock paketålder ist
+            if (_authorized_edge[i].last_seq == seq) // check duplicate sequence
                 return false;
-            /*
-            if (seq <= _authorized_edge[i].last_seq) //real code when provitioning correct
-                return false; // duplicate sequence
-            */
-            //_authorized_edge[i].last_seq = seq;
-            
-
-
+         
             // --- HMAC verification ---
             SHA256 sha;
             uint8_t full_hash[32];
@@ -107,6 +86,7 @@ bool AuthNode::validateEdge(edge_event_t* pkt) {
             sha.update((uint8_t*)pkt, offsetof(edge_event_t,  auth_tag));
             sha.finalizeHMAC(_authorized_edge[i].shared_key, KEY_LEN, full_hash, AUTH_TAG_LEN);
             memcpy(computed_tag, full_hash, AUTH_TAG_LEN);
+
             // compare computed HMAC with received auth_tag
             if (!constTimeComp(pkt->auth_tag, computed_tag, AUTH_TAG_LEN)) {
                 Serial.println("Auth tag mismatch!");
@@ -163,9 +143,48 @@ bool AuthNode::removeEdge(uint8_t device_id) {
     return false;
 }
 
+
+
 void AuthNode::persistEdge(uint8_t index) {
     if (index >= MAX_APPROVED_EDGE) return;
 
     int addr = EEPROM_START + index * sizeof(eeprom_edge_t);
     EEPROM.put(addr, _authorized_edge[index]);
+
 }
+
+
+
+//test start
+/*
+void AuthNode::begin(uint8_t node_id) { //fake for key test, shold be remove later
+
+    _node_id = node_id;
+
+    // Clear all authorized edges first
+    for (int i = 0; i < MAX_APPROVED_EDGE; i++) {
+        _authorized_edge[i].device_id = 0xFF;  // mark unused
+        _authorized_edge[i].last_seq = 0xFF;
+        _authorized_edge[i].key_timestamp = 1;
+    }
+
+    // ----- FAKE FOG PROVISIONING -----
+
+    // We authorize ONE edge device (device_id = 1)
+    _authorized_edge[0].device_id = 1;
+
+    uint8_t fake_key[KEY_LEN] = {
+        0x9A, 0x4F, 0x21, 0xC7,
+        0x55, 0x13, 0xE8, 0x02,
+        0x6D, 0xB9, 0x33, 0xA1,
+        0x7C, 0x4D, 0x90, 0xEE
+    };
+
+    memcpy(_authorized_edge[0].shared_key, fake_key, KEY_LEN);
+
+    _authorized_edge[0].last_seq = 0xFF;
+
+    Serial.println("Edge provisioned (device_id = 1)");
+}
+*/
+//test end
